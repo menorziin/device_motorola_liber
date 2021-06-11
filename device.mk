8ea4b790afda7f3db30ec33bbc959b258bdfc9cd
menorziin <williamsilvapppl@gmail.com>
2021-06-11 00:01:35 -0300

    liber: Add lights hal
    * Based in motorola sdm632

--- device.mk
+++ device.mk
@@ -28,7 +28,11 @@ $(call inherit-product, vendor/motorola/liber/liber-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -50,6 +54,10 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc \
     init.oem.fingerprint.sh \
     init.oem.fingerprint2.sh
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.liber
 
 # Input
 PRODUCT_COPY_FILES += \
