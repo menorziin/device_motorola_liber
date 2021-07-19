c0c55e5e2f894dbe0df2143327832cd513c307b2
menorziin <williamsilvapppl@gmail.com>
2021-07-19 12:34:41 +0000

    liber: import vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/LineageOS/android_hardware_qcom_display/ -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
+
+git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_whitney.git -b
+eleven-arm64 packages/apps/MotCamera2
+
+git clone
+https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_overlay.git -b ten
+packages/apps/MotCamera2-overlay
+
+git clone https://gitlab.com/NemesisDevelopers/motorola/motorola_motosignatureapp.git -b eleven packages/apps/MotoSignatureApp
