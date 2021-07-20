e32135e74e136f25e24329831fe3fbe4824f88dc
William <71452169+menorziin@users.noreply.github.com>
2021-07-20 04:56:14 -0300

    liber: Import hardware/libhardware from Lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,7 @@
 rm -rf hardware/qcom-caf/sm8150/display && git clone https://github.com/LineageOS/android_hardware_qcom_display/ -b lineage-18.1-caf-sm8150 hardware/qcom-caf/sm8150/display
 
+rm -rf hardware/libhardware && git clone https://github.com/LineageOS/android_hardware_libhardware -b lineage-18.1 hardware/libhardware
+
 git clone https://gitlab.com/NemesisDevelopers/moto-camera/motorola_camera2_whitney.git -b
 eleven-arm64 packages/apps/MotCamera2
 
