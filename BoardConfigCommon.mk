526a455f1b8180e1636fa17d273e78cc128ca3d6
npjohnson <johnsonnolen@gmail.com>
2022-02-13 00:11:28 +0530

    sm6150-common: Simplify AVB flag logic
    * AVB's `--flags 3` arguement builds a disabled VBMeta with
      disabled hashtree, so no need to set both flags individually.
    
    Change-Id: Ia7060c2eb10dd5a16e3323dc543f6e6f00a156f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -243,8 +243,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
