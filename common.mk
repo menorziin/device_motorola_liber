55092fc2d26c4dbe4aee65be154f6cefe581850c
dianlujitao <dianlujitao@lineageos.org>
2022-03-16 17:05:56 +0800

    sm6150-common: Drop Snap
    Change-Id: Ie5e2c3d337a3d80795d6d307eb5cc4326d73bbd0

--- common.mk
+++ common.mk
@@ -34,8 +34,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # A/B
 AB_OTA_UPDATER := true
@@ -168,7 +167,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     android.frameworks.displayservice@1.0.vendor
