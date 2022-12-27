d9c91e00680c54d4c2a2ea27b39a605aba7e5dd1
Omkar Chandorkar <gotenksIN@aospa.co>
2022-12-27 18:04:41 +0900

    liber: overlay: Import CarrierConfig from QSSI
    Change-Id: Iee81635fdbe4c213b710bccfb6ebe6e0381180e8

--- device.mk
+++ device.mk
@@ -34,12 +34,12 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     FrameworksResCommon_Sys \
-    CarrierConfigResCommon_Sys \
     SystemUIResCommon_Sys \
     TelephonyResCommon_Sys \
     WifiResCommon_Sys \
 
 PRODUCT_PACKAGES += \
+    CarrierConfig \
     FrameworksResLiber \
     SettingsProviderResLiber \
     SettingsResLiber \
