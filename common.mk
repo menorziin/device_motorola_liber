7aa8b9395fe5c3287b1287d7ef0a84b75c53d7e3
menorziin <williamsilvapppl@gmail.com>
2022-12-27 05:34:56 -0300

    sm6150-common: Add back livedisplay
    This reverts commit dfeed1be66029a0e3f09c0304938ff5f01065956.
    
    Change-Id: If33e0c045030f6c54f70cfb9584337b45b9daafb

--- common.mk
+++ common.mk
@@ -293,6 +293,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs.motorola_sm6150
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
