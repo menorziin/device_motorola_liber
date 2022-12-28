eccc2cb9839b779989526c4ac0d76e440524d767
menorziin <williamsilvapppl@gmail.com>
2022-12-28 00:59:20 -0300

    sm6150-common: manifest: Add FCM to satisfy vintf check
    - The following HALs in device manifest are not declared in FCM <= level 4
    - Generated by: https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Change-Id: Ia6afe26cf53611d4cc585e5fbe995993877d5cd2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,6 +175,7 @@ TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
